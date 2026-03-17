8fe0014a49707758e4db67170dc9f559683944b4
Daniel Podrażka <podrazkafreelance@gmail.com>
2026-03-17 22:24:14 +0000

    Increase stream rate from 200Hz to 500Hz (2ms timer)
    Halves the interval from 5ms to 2ms, giving 2.5x more travel
    samples per keypress for better velocity accuracy while leaving
    headroom below the 1kHz USB Full-Speed ceiling.

--- keyboards/keychron/common/analog_matrix/analog_matrix.c
+++ keyboards/keychron/common/analog_matrix/analog_matrix.c
@@ -875,7 +875,7 @@ void analog_matrix_task(void) {
 #endif
 
     // Stream travel data at ~200Hz (every 5ms)
-    if (stream_enabled && timer_elapsed32(stream_timer) >= 5) {
+    if (stream_enabled && timer_elapsed32(stream_timer) >= 2) {
         stream_timer = timer_read32();
         stream_travel_report();
     }
